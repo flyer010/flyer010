6585cd88bdb3b36fdd2fcb8fe4447150e6d73a27
flyer010 <1205031994@qq.com>
2021-06-23 16:31:38 +0800

    Revert "第4次提交代码"
    This reverts commit 3cfb242b

--- spring_boot/src/com/itheima/HelloWorld.java
+++ spring_boot/src/com/itheima/HelloWorld.java
@@ -5,7 +5,5 @@ public class HelloWorld {
 
         System.out.println("hello git!");
         System.out.println("hello git!");
-        System.out.println("hello git!");
-        System.out.println("hello git!！！！！！！！");
     }
 }
